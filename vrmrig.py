b027fd473efa6fd63938e7bcbf87bc6fe58ea9cb
eclipseAce <vcsdk2468@gmail.com>
2024-03-27 16:58:38 +0800

    first commit

--- vrmrig.py
+++ vrmrig.py
@@ -25,12 +25,6 @@ class ArmatureSupport:
             bone = edit_bones.new(bonename)
         return bone
     
-    def get_pose_bone(self, bonename):
-        return self.armature.pose.bones.get(bonename)
-    
-    def get_bone(self, bonename):
-        return self.armature.data.bones.get(bonename)
-    
     def get_bone_constraint(self, bonename, type, name):
         b = self.armature.pose.bones[bonename]
         c = next((c for c in b.constraints if c.name == name), None)
@@ -38,24 +32,29 @@ class ArmatureSupport:
             c = b.constraints.new(type)
             c.name = name
         return c
+    
+    def set_bone_layer(self, bonenames, layer):
+        layers = [i == layer for i in range(32)]
+        for bonename in bonenames:
+            self.armature.data.bones[bonename].layers = layers.copy()
 
 class FingerCtrl:
-    bone_pattern = re.compile("^(Thumb|Index|Middle|Ring|Little)(1|2|3)_(L|R)$")
-
     def __init__(self, armature, side, finger, bending_axis):
         self.armature = ArmatureSupport(armature)
-        self.bonename1 = f"{finger}1_{side}"
-        self.bonename2 = f"{finger}2_{side}"
-        self.bonename3 = f"{finger}3_{side}"
-        self.hand_bonename = f"Hand_{side}"
-        self.ctrl_bonename = f"{finger}Rig_{side}"
+        self.bone1 = f"{finger}1_{side}"
+        self.bone2 = f"{finger}2_{side}"
+        self.bone3 = f"{finger}3_{side}"
+        self.hand = f"Hand_{side}"
+        self.ctrl = f"{finger}Ctrl_{side}"
         self.bending_axis = bending_axis
     
     def new_bones(self):
-        bone = self.armature.get_edit_bone(self.ctrl_bonename, True)
-        bone.head = self.armature.get_edit_bone(self.bonename1).head
-        bone.tail = self.armature.get_edit_bone(self.bonename3).tail
-        bone.parent = self.armature.get_edit_bone(self.hand_bonename)
+        bone1 = self.armature.get_edit_bone(self.bone1)
+        bone3 = self.armature.get_edit_bone(self.bone3)
+        bone = self.armature.get_edit_bone(self.ctrl, True)
+        bone.head = bone1.head
+        bone.tail = bone.head + (bone3.tail - bone1.head) * 1.5
+        bone.parent = self.armature.get_edit_bone(self.hand)
 
     def rotation_follow(self, follower, followed):
         c = self.armature.get_bone_constraint(follower, 'COPY_ROTATION', 'VRMRIG_RotationFollow')
@@ -65,48 +64,44 @@ class FingerCtrl:
         c.owner_space = 'LOCAL'
     
     def limit_ctrl_scale(self):
-        c = self.armature.get_bone_constraint(self.ctrl_bonename, 'LIMIT_SCALE', 'VRMRIG_LimitScale')
+        c = self.armature.get_bone_constraint(self.ctrl, 'LIMIT_SCALE', 'VRMRIG_LimitScale')
         c.use_min_x = c.use_min_y = c.use_min_z = True
         c.use_max_x = c.use_max_y = c.use_max_z = True
-        c.min_y = 0
         c.min_x = c.min_z = 1
         c.max_x = c.max_z = c.max_y = 1
+        c.min_y = 0.5
         c.owner_space = 'LOCAL'
     
     def rotation_driver(self, bonename, axis):
         index  = {'X':1, '-X':1, 'Y':2, '-Y':2, 'Z':3, '-Z':3}[axis]
-        bone = self.armature.get_pose_bone(bonename)
+        bone = self.armature.armature.pose.bones[bonename]
         bone.driver_remove('rotation_quaternion', index)
         drv = bone.driver_add('rotation_quaternion', index).driver
         drv.type = 'SCRIPTED'
-        drv.expression = 'var - 1' if axis.startswith('-') else '1 - var'
+        drv.expression = '(var - 1) * 2'
+        if axis.startswith('-'):
+            drv.expression = '-' + drv.expression
         var = drv.variables.new()
         var.name = "var"
         var.type = 'TRANSFORMS'
         t = var.targets[0]
         t.id = self.armature.armature
-        t.bone_target = self.ctrl_bonename
+        t.bone_target = self.ctrl
         t.transform_type = 'SCALE_Y'
         t.transform_space = 'LOCAL_SPACE'
     
-    def set_bone_layer(self, bonenames, layer):
-        layers = [i == layer for i in range(32)]
-        for bonename in bonenames:
-            self.armature.get_bone(bonename).layers = layers.copy()
-    
     def generate(self):
         self.armature.edit_mode(self.new_bones)
-        self.rotation_follow(self.bonename3, self.bonename2)
-        self.rotation_follow(self.bonename1, self.ctrl_bonename)
-        self.rotation_driver(self.bonename2, self.bending_axis)
+        self.rotation_follow(self.bone3, self.bone2)
+        self.rotation_follow(self.bone1, self.ctrl)
+        self.rotation_driver(self.bone2, self.bending_axis)
         self.limit_ctrl_scale()
-        self.set_bone_layer([self.bonename1, self.bonename2, self.bonename3], 1)
+        self.armature.set_bone_layer([self.bone1, self.bone2, self.bone3], 1)
 
 
 class LimbsCtrl:
     def __init__(self, armature, side):
         self.armature = ArmatureSupport(armature)
-        self.side = side
         self.root_bonename = 'Root'
         self.hand_bonename = f"Hand_{side}"
         self.lowerarm_bonename = f"LowerArm_{side}"
@@ -117,38 +112,29 @@ class LimbsCtrl:
         self.legpole_bonename = f"LegPole_{side}"
     
     def new_bones(self):
-        direction = 1.0 if self.side == 'L' else -1.0
-        
         root = self.armature.get_edit_bone(self.root_bonename)
         lowerarm = self.armature.get_edit_bone(self.lowerarm_bonename)
         lowerleg = self.armature.get_edit_bone(self.lowerleg_bonename)
 
         bone = self.armature.get_edit_bone(self.armik_bonename, True)
         bone.head = lowerarm.tail
-        bone.tail = bone.head + Vector((0.12*direction, 0, 0))
+        bone.tail = bone.head + Vector((0, 0.12, 0))
         bone.parent = root
 
         bone = self.armature.get_edit_bone(self.armpole_bonename, True)
         bone.head = lowerarm.head + Vector((0, 0.24, 0))
-        bone.tail = bone.head + Vector((0, 0.12*direction, 0))
+        bone.tail = bone.head + Vector((0, 0.12, 0))
         bone.parent = root
 
         bone = self.armature.get_edit_bone(self.legik_bonename, True)
         bone.head = lowerleg.tail
-        bone.tail = bone.head + Vector((0.12*direction, 0, 0))
+        bone.tail = bone.head + Vector((0, 0.12, 0))
         bone.parent = root
 
         bone = self.armature.get_edit_bone(self.legpole_bonename, True)
         bone.head = lowerleg.head - Vector((0, 0.24, 0))
-        bone.tail = bone.head - Vector((0, 0.12*direction, 0))
+        bone.tail = bone.head - Vector((0, 0.12, 0))
         bone.parent = root
-    
-    def hand_transforms_follow(self):
-        c = self.armature.get_bone_constraint(self.hand_bonename, 'COPY_TRANSFORMS', 'VRMRIG_TransformsFollow')
-        c.target = self.armature.armature
-        c.subtarget = self.armik_bonename
-        c.target_space = 'WORLD'
-        c.owner_space = 'WORLD'
 
     def ik(self, bonename, ik_bonename, pole_bonename, pole_angle, constraint_name):
         c = self.armature.get_bone_constraint(bonename, 'IK', constraint_name)
@@ -161,25 +147,155 @@ class LimbsCtrl:
     
     def generate(self):
         self.armature.edit_mode(self.new_bones)
-        self.hand_transforms_follow()
-        self.ik(self.lowerarm_bonename, self.armik_bonename, self.armpole_bonename, 180 if self.side == 'L' else 0, 'VRMRIG_ArmIK')
+        
+        arm = self.armature.armature.data.bones[self.lowerarm_bonename]
+        dir = (arm.tail - arm.head).to_2d().cross(Vector((0, 1)))
+        self.ik(self.lowerarm_bonename, self.armik_bonename, self.armpole_bonename, 180 if dir > 0 else 0, 'VRMRIG_ArmIK')
         self.ik(self.lowerleg_bonename, self.legik_bonename, self.legpole_bonename, -90, 'VRMRIG_LegIK')
 
-class Hair:
-    bone_pattern = re.compile("^(Hair|Bust)\\d+_(L|R|\d+)$")
-    
-    def __init__(self, armature):
+class SpringBone:
+    def __init__(self, armature, bone_pattern):
         self.armature = ArmatureSupport(armature)
+        self.bone_pattern = re.compile(bone_pattern)
     
-    def enable_spring_bone(self):
+    def enable(self):
         for bonename, bone in self.armature.armature.pose.bones.items():
-            if Hair.bone_pattern.match(bonename) is None:
+            if self.bone_pattern.match(bonename) is None:
                 continue
             bone.sb_bone_spring = True
             bone.sb_bone_rot = True
             bone.sb_stiffness = 0.3
             bone.sb_gravity = 0
             bone.sb_damp = 0.5
+
+class ArmTwist:
+    def __init__(self, armature, side):
+        self.armature = ArmatureSupport(armature)
+        self.hand = f"Hand_{side}"
+        self.arm = f"LowerArm_{side}"
+        self.subarms = [f"LowerArm{n+1}_{side}" for n in range(3)]
+    
+    def new_bones(self):
+        arm = self.armature.get_edit_bone(self.arm)
+        v = (arm.tail - arm.head) / len(self.subarms)
+        for i, name in enumerate(self.subarms):
+            bone = self.armature.get_edit_bone(name, True)
+            bone.head = arm.head + i * v
+            bone.tail = bone.head + v
+            bone.parent = arm
+            bone.use_deform = True
+    
+    def rotation_follow(self, follower, followed, influence):
+        c = self.armature.get_bone_constraint(follower, 'COPY_ROTATION', 'VRMRIG_ArmTwistFollow')
+        c.target = self.armature.armature
+        c.subtarget = followed
+        c.use_x = c.use_z = False
+        c.target_space = 'LOCAL'
+        c.owner_space = 'LOCAL'
+        c.influence = influence
+    
+    def generate(self):
+        self.armature.edit_mode(self.new_bones)
+        
+        followed = self.subarms[1:] + [self.hand]
+        for i, n in enumerate(self.subarms):
+            influcence = (i + 1) * (0.9 / len(self.subarms))
+            self.rotation_follow(n, followed[i], influcence)
+        
+        self.armature.armature.data.bones[self.arm].use_deform = False
+        
+        prevmode = self.armature.armature.mode
+        prevactive = bpy.context.view_layer.objects.active
+        try:
+            for mesh in bpy.data.objects:
+                if mesh.type != 'MESH' or mesh.parent != self.armature.armature:
+                    continue
+                bpy.context.view_layer.objects.active = mesh
+                
+                vg = mesh.vertex_groups.get(self.arm)
+                if vg != None:
+                    mesh.vertex_groups.remove(vg)
+                
+                bones = self.armature.armature.data.bones
+                bpy.ops.object.mode_set(mode = 'WEIGHT_PAINT')
+                prevselect = { b.name: b.select for b in bones }
+                for n in self.subarms:
+                    bones[n].select = True
+                bpy.ops.paint.weight_from_bones(type = 'AUTOMATIC')
+                for n, s in prevselect.items():
+                    bones[n].select = s
+        finally:
+            bpy.ops.object.mode_set(mode = prevmode)
+            bpy.context.view_layer.objects.active = prevactive
+        
+        self.armature.set_bone_layer(self.subarms, 1)
+
+class ShoulderCtrl:
+    def __init__(self, armature, side):
+        self.armature = ArmatureSupport(armature)
+        self.hand = f"Hand_{side}"
+        self.arm = f"LowerArm_{side}"
+        self.shoulder = f"Shoulder_{side}"
+        self.twist = f"ShoulderTwist_{side}"
+        self.ctrl = f"ShoulderCtrl_{side}"
+    
+    def new_bones(self):
+        shoulder = self.armature.get_edit_bone(self.shoulder)
+        arm = self.armature.get_edit_bone(self.arm)
+        hand = self.armature.get_edit_bone(self.hand)
+
+        twist = self.armature.get_edit_bone(self.twist, True)
+        twist.head = shoulder.head
+        twist.tail = twist.head + (shoulder.tail - shoulder.head) * 0.7
+        twist.parent = shoulder.parent
+        shoulder.parent = twist
+
+        ctrl = self.armature.get_edit_bone(self.ctrl, True)
+        ctrl.head = shoulder.tail + (shoulder.tail - shoulder.head).normalized() * (arm.head - shoulder.tail).length
+        ctrl.tail = ctrl.head + Vector((0, 0.12, 0))
+        ctrl.parent = hand
+    
+    def damped_follow(self, follower, followed, influence):
+        c = self.armature.get_bone_constraint(follower, 'DAMPED_TRACK', 'VRMRIG_ShoulderFollow')
+        c.target = self.armature.armature
+        c.subtarget = followed
+        c.track_axis = 'TRACK_Y'
+        c.influence = influence
+        
+    def generate(self):
+        self.armature.edit_mode(self.new_bones)
+        self.damped_follow(self.twist, self.ctrl, 0.5)
+        
+
+class VRMRIG_FixArmTwist(bpy.types.Operator):
+    bl_idname = "vrmrig.fix_arm_twist"
+    bl_label = "Fix arm twist"
+    bl_description = "VRMRIG: Fix arm twist"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None and context.active_object.type == 'ARMATURE'
+
+    def execute(self, context):
+        armature = context.active_object
+        ArmTwist(armature, 'L').generate()
+        ArmTwist(armature, 'R').generate()
+        return {'FINISHED'}
+
+class VRMRIG_AddShoulderControllerOperator(bpy.types.Operator):
+    bl_idname = "vrmrig.add_shoulder_ctrl"
+    bl_label = "Add shoulder controller"
+    bl_description = "VRMRIG: Add shoulder controller"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None and context.active_object.type == 'ARMATURE'
+
+    def execute(self, context):
+        armature = context.active_object
+        ShoulderCtrl(armature, 'L').generate()
+        ShoulderCtrl(armature, 'R').generate()
+        return {'FINISHED'}
 
 class VRMRIG_AddControllerOperator(bpy.types.Operator):
     bl_idname = "vrmrig.add_controller"
@@ -194,21 +310,21 @@ class VRMRIG_AddControllerOperator(bpy.types.Operator):
         armature = context.active_object
         LimbsCtrl(armature, 'L').generate()
         LimbsCtrl(armature, 'R').generate()
-        FingerCtrl(armature, 'L', 'Thumb', 'Z').generate()
-        FingerCtrl(armature, 'R', 'Thumb', '-Z').generate()
+        FingerCtrl(armature, 'L', 'Thumb', '-Z').generate()
+        FingerCtrl(armature, 'R', 'Thumb', 'Z').generate()
         FingerCtrl(armature, 'L', 'Index', 'X').generate()
-        FingerCtrl(armature, 'R', 'Index', '-X').generate()
+        FingerCtrl(armature, 'R', 'Index', 'X').generate()
         FingerCtrl(armature, 'L', 'Middle', 'X').generate()
-        FingerCtrl(armature, 'R', 'Middle', '-X').generate()
+        FingerCtrl(armature, 'R', 'Middle', 'X').generate()
         FingerCtrl(armature, 'L', 'Ring', 'X').generate()
-        FingerCtrl(armature, 'R', 'Ring', '-X').generate()
+        FingerCtrl(armature, 'R', 'Ring', 'X').generate()
         FingerCtrl(armature, 'L', 'Little', 'X').generate()
-        FingerCtrl(armature, 'R', 'Little', '-X').generate()
+        FingerCtrl(armature, 'R', 'Little', 'X').generate()
         return {'FINISHED'}
 
-class VRMRIG_EnableHairSpringBoneOperator(bpy.types.Operator):
-    bl_idname = "vrmrig.enable_hair_spring_bone"
-    bl_label = "Enable hair spring bone"
+class VRMRIG_EnableSpringBoneOperator(bpy.types.Operator):
+    bl_idname = "vrmrig.enable_spring_bone"
+    bl_label = "Enable spring bone"
     bl_description = "VRMRIG: Enable hair spring bone"
 
     @classmethod
@@ -217,7 +333,7 @@ class VRMRIG_EnableHairSpringBoneOperator(bpy.types.Operator):
 
     def execute(self, context):
         armature = context.active_object
-        Hair(armature).enable_spring_bone()
+        SpringBone(armature, "^(Hair|Bust)\\d+_(L|R|\d+)$").enable()
         return {'FINISHED'}
 
 class VRMRIG_Panel(bpy.types.Panel):
@@ -231,18 +347,27 @@ class VRMRIG_Panel(bpy.types.Panel):
         layout = self.layout
         row = layout.row()
         row.operator("vrmrig.add_controller")
-        row.operator("vrmrig.enable_hair_spring_bone")
+        row = layout.row()
+        row.operator("vrmrig.enable_spring_bone")
+        row = layout.row()
+        row.operator("vrmrig.fix_arm_twist")
+        row = layout.row()
+        row.operator("vrmrig.add_shoulder_ctrl")
 
 
 def register():
     bpy.utils.register_class(VRMRIG_AddControllerOperator)
-    bpy.utils.register_class(VRMRIG_EnableHairSpringBoneOperator)
+    bpy.utils.register_class(VRMRIG_EnableSpringBoneOperator)
+    bpy.utils.register_class(VRMRIG_FixArmTwist)
+    bpy.utils.register_class(VRMRIG_AddShoulderControllerOperator)
     bpy.utils.register_class(VRMRIG_Panel)
 
 
 def unregister():
     bpy.utils.unregister_class(VRMRIG_Panel)
-    bpy.utils.unregister_class(VRMRIG_EnableHairSpringBoneOperator)
+    bpy.utils.unregister_class(VRMRIG_AddShoulderControllerOperator)
+    bpy.utils.unregister_class(VRMRIG_FixArmTwist)
+    bpy.utils.unregister_class(VRMRIG_EnableSpringBoneOperator)
     bpy.utils.unregister_class(VRMRIG_AddControllerOperator)
 
 
