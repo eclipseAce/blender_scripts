3424f3009ec041b6c96e9a74772b8b77be03a115
eclipseAce <vcsdk2468@gmail.com>
2024-03-30 00:48:29 +0800

    add bone shapes

--- vrmauto.py
+++ vrmauto.py
@@ -1,45 +1,98 @@
 import bpy
 import math
 import re
-from bpy.types import Object, Armature, Bone, PoseBone, Constraint
-from mathutils import Vector, Euler
+from typing import List
+from bpy.types import Object, Armature, Bone, PoseBone, Constraint, Collection, LimitRotationConstraint
+from mathutils import Vector, Euler, Quaternion
 
-def add_bone_constraint(b:PoseBone, name:str, type:str, **props) -> Constraint:
+def add_constraint(b:Object|PoseBone, name:str, type:str, **props) -> Constraint:
     c = next((c for c in b.constraints if c.name == name), None)
     if c is None:
         c = b.constraints.new(type)
-        print(c)
         c.name = name
     for k, v in props.items():
         c.__setattr__(k, v)
     return c
 
+def disable_rotation(b:Object|PoseBone) -> Constraint:
+    c:LimitRotationConstraint = add_constraint(b, 'VRMAUTO_DisableRotation', 'LIMIT_ROTATION')
+    c.use_limit_x = c.use_limit_y = c.use_limit_z = True
+    c.max_x = c.max_y = c.max_z = c.min_x = c.min_y = c.min_z = 0
+
 def set_bone_layer(armature:Armature, layer:int, *names) -> None:
     for n in names:
         armature.bones[n].layers = [i == layer for i in range(32)]
 
-def get_finger_bone_shape() -> Object:
-    verts = [ (0, 0, 0), (0, 0.9, 0), (-0.05, 0.9, 0), (-0.05, 1, 0), (0.05, 1, 0), (0.05, 0.9, 0) ]
-    edges = [ (0, 1), (2, 3), (3, 4), (4, 5), (5, 2) ]
-    
+def get_bone_shape_collection() -> Collection:
     coll = bpy.data.collections.get("VRMAUTO")
     if coll == None:
         coll = bpy.data.collections.new("VRMAUTO")
         bpy.context.scene.collection.children.link(coll)
-    coll.hide_select = True
-    coll.hide_viewport = True
-    coll.hide_render = True
-    bpy.context.view_layer.layer_collection.children["VRMAUTO"].exclude = True
+        coll.hide_select = True
+        coll.hide_viewport = True
+        coll.hide_render = True
+        bpy.context.view_layer.layer_collection.children["VRMAUTO"].exclude = True
+    return coll
 
-    name = f"VRMAUTO_FingerBoneShape"
+def get_bone_shape(name:str, verts:List, edges:List) -> Object:
     obj = bpy.data.objects.get(name)
     if obj == None:
         mesh = bpy.data.meshes.new(name)
         mesh.from_pydata(verts, edges, [])
         mesh.update()
         obj = bpy.data.objects.new(name, mesh)
+        coll = get_bone_shape_collection()
         coll.objects.link(obj)
     return obj
+
+def get_finger_bone_shape() -> Object:
+    return get_bone_shape(
+        "VRMAUTO_FingerBoneShape",
+        [ (0, 0, 0), (0, 0.9, 0), (-0.05, 0.9, 0), (-0.05, 1, 0), (0.05, 1, 0), (0.05, 0.9, 0) ],
+        [ (0, 1), (2, 3), (3, 4), (4, 5), (5, 2) ],
+    )
+
+def get_hand_bone_shape() -> Object:
+    return get_bone_shape(
+        "VRMAUTO_HandBoneShape",
+        [ (0.66, 1, 0), (0.66, 0, 0), (-0.66, 0, 0), (-0.66, 1, 0) ],
+        [ (0, 1), (1, 2), (2, 3), (3, 0) ],
+    )
+
+def get_pole_bone_shape() -> Object:
+    name = "VRMAUTO_PoleBoneShape"
+    obj = bpy.data.objects.get(name)
+    if obj == None:
+        obj = bpy.data.objects.new(name, None)
+        obj.empty_display_size = 0.5
+        obj.empty_display_type = 'PLAIN_AXES'
+    return obj
+
+def get_leg_ik_bone_shape() -> Object:
+    name = "VRMAUTO_LegIKBoneShape"
+    obj = bpy.data.objects.get(name)
+    if obj == None:
+        obj = bpy.data.objects.new(name, None)
+        obj.empty_display_size = 0.25
+        obj.empty_display_type = 'CUBE'
+    return obj
+
+def get_root_bone_shape() -> Object:
+    return get_bone_shape(
+        "VRMAUTO_RootBoneShape",
+        [
+            (0.3, 0, 0.5), (0.5, 0, 0.5), (0.5, 0, 0.3),
+            (-0.3, 0, -0.5), (-0.5, 0, -0.5), (-0.5, 0, -0.3),
+            (-0.3, 0, 0.5), (-0.5, 0, 0.5), (-0.5, 0, 0.3),
+            (0.3, 0, -0.5), (0.5, 0, -0.5), (0.5, 0, -0.3),
+        ],
+        [
+            (0, 1), (1, 2),
+            (3, 4), (4, 5),
+            (6, 7), (7, 8),
+            (9, 10), (10, 11),
+        ],
+    )
 
 def symmetrize_bone_names(obj:Object):
     if obj is None or obj.type != 'ARMATURE':
@@ -111,12 +164,11 @@ def gen_finger_ctrl(obj:Object, finger:str, side:str, bending_axis:str):
             pbone.custom_shape_rotation_euler = Euler((0, 1, 0))
         else:
             pbone.custom_shape_rotation_euler = Euler((0, 0, 0))
-
     edit_bones()
     
     # add rotation constraints
     def set_rotation_follow(bname1, bname2):
-        add_bone_constraint(
+        add_constraint(
             obj.pose.bones[bname1], 'VRMAUTO_BendingFollow', 'COPY_ROTATION',
             target = obj,
             subtarget = bname2,
@@ -128,7 +180,7 @@ def gen_finger_ctrl(obj:Object, finger:str, side:str, bending_axis:str):
 
     # limit scale of control bone
     def set_limit_scale(bname):
-        c = add_bone_constraint(
+        c = add_constraint(
             obj.pose.bones[bname], 'VRMAUTO_ScaleLimit', 'LIMIT_SCALE',
             owner_space = 'LOCAL'
         )
@@ -177,6 +229,9 @@ def gen_limbs_ik(obj:Object, kind:str, side:str):
     for n in [root_name, upper_name, lower_name]:
         if armature.bones.get(n) is None:
             raise AssertionError(f"missing bone '{n}'")
+    
+    def is_left() -> bool:
+        return armature.bones[lower_name].vector.to_2d().cross(Vector((0, 1))) > 0
 
     def edit_bones():
         prevmode = obj.mode
@@ -184,21 +239,46 @@ def gen_limbs_ik(obj:Object, kind:str, side:str):
         try:
             bones = armature.edit_bones
             root, upper, lower = (bones[n] for n in [root_name, upper_name, lower_name])
-            if upper.vector.cross(lower.vector) == 0:
-                raise AssertionError(f"bone '{upper_name}' and '{lower_name}' bone are in parallel")
             ik, pole = (bones.get(n) or bones.new(n) for n in [ik_name, pole_name])
+
+            ik_size = 0.24 # default hand length
+            if kind == 'Arm':
+                # determin hand length by finding closest finger
+                finger_names = [f"{n}1_{side}" for n in ['Index', 'Middle', 'Ring', 'Little']]
+                finger_bones = [b for b in [bones.get(n) for n in finger_names] if b != None]
+                if len(finger_bones) > 0:
+                    ik_size = min([(b.head - lower.tail).length for b in finger_bones])
+
             ik.head = lower.tail
-            ik.tail = ik.head + lower.vector.normalized() * 0.12
-            pole_v = ((upper.vector + (-lower.vector)) / 2).normalized()
-            pole.head = lower.head + pole_v * 0.24
+            ik.tail = ik.head + lower.vector.normalized() * ik_size
+
+            # determin arm pole by the angle between upper and lower arms, they must not be in parallel
+            base_v = Vector((1, 0, 0))
+            if kind == 'Leg':
+                base_v = Vector((0, 0, 1))
+            pole_v:Vector = upper.vector.cross(lower.vector)
+            if pole_v.length == 0:
+                raise AssertionError(f"bone '{upper_name}' and '{lower_name}' bone are in parallel")
+            pole_v.rotate(Quaternion(base_v, math.radians(90.0)))
+            pole_v.rotate(Vector(base_v).rotation_difference(lower.tail - upper.head))
+            pole_v.normalize()
+
+            # pole is 1.1 times length of upper bone away from joint, and the size is hardcoded as 0.12
+            pole.head = lower.head + pole_v * (upper.vector.length * 1.1) 
             pole.tail = pole.head + pole_v * 0.12
             ik.parent = pole.parent = root
         finally:
             bpy.ops.object.mode_set(mode = prevmode)
     edit_bones()
 
+    # change pole bone shape
+    pole_pbone:PoseBone = obj.pose.bones[pole_name]
+    pole_pbone.custom_shape = get_pole_bone_shape()
+    disable_rotation(pole_pbone)
+
+    # set ik
     def set_ik():
-        c = add_bone_constraint(
+        c = add_constraint(
             obj.pose.bones[lower_name], 'VRMAUTO_ArmIK', 'IK',
             target = obj,
             subtarget = ik_name,
@@ -207,22 +287,44 @@ def gen_limbs_ik(obj:Object, kind:str, side:str):
             chain_count = 2,
         )
         if kind == 'Arm':
-            cross = armature.bones[lower_name].vector.to_2d().cross(Vector((0, 1)))
-            c.pole_angle = math.radians(180 if cross > 0 else 0)
+            c.pole_angle = math.radians(180 if is_left() else 0)
         else:
             c.pole_angle = math.radians(-90)
     set_ik()
     
-    # set hand bone follows arm ik bone
     if kind == 'Arm':
+        # set hand bone follows arm ik bone
         hand_name = f"Hand_{side}"
-        add_bone_constraint(
+        add_constraint(
             obj.pose.bones[hand_name], 'VRAMAUTO_FollowArmIK', 'COPY_TRANSFORMS',
             target = obj,
             subtarget = ik_name,
         )
+
+        # set original hand bone to layer 1
         set_bone_layer(armature, 1, hand_name)
-        
+
+        # change hand ik bone shape
+        pbone:PoseBone = obj.pose.bones[ik_name]
+        pbone.custom_shape = get_hand_bone_shape()
+
+        # hand ik bone rotation limit
+        c:LimitRotationConstraint = add_constraint(pbone, 'VRMAUTO_HandRotationLimit', 'LIMIT_ROTATION')
+        c.use_limit_x = c.use_limit_z = True
+        c.min_x = math.radians(-80)
+        c.max_x = math.radians(80)
+        if is_left():
+            c.min_z = math.radians(-10)
+            c.max_z = math.radians(45)
+        else:
+            c.min_z = math.radians(-45)
+            c.max_z = math.radians(10)
+        c.owner_space = 'LOCAL'
+    else:
+        # change leg ik bone shape
+        pbone:PoseBone = obj.pose.bones[ik_name]
+        pbone.custom_shape = get_leg_ik_bone_shape()
+        disable_rotation(pbone)
 
 def fix_arm_twist(obj:Object, side:str):
     if obj is None or obj.type != 'ARMATURE':
@@ -253,7 +355,7 @@ def fix_arm_twist(obj:Object, side:str):
 
     # set rotation follow for each sub arm bone
     def set_rotation_follow(bname1, bname2, influence):
-        add_bone_constraint(
+        add_constraint(
             obj.pose.bones[bname1], 'VRMAUTO_ArmTwistFollow', 'COPY_ROTATION',
             target = obj,
             subtarget = bname2,
@@ -299,6 +401,13 @@ def fix_arm_twist(obj:Object, side:str):
     # move sub arm bones to layer 1
     set_bone_layer(armature, 1, *subarm_names)
 
+def change_root_bone_shape(obj:Object):
+    if obj is None or obj.type != 'ARMATURE':
+        raise TypeError("expected a obj type of ARMATURE")
+    root_name = "Root"
+    pbone:PoseBone = obj.pose.bones[root_name]
+    pbone.custom_shape = get_root_bone_shape()
+
 def set_spring_bones(obj:Object):
     if obj is None or obj.type != 'ARMATURE':
         raise TypeError("expected a obj type of ARMATURE")
@@ -317,14 +426,15 @@ def set_spring_bones(obj:Object):
 obj = bpy.context.active_object
 if obj != None and obj.type == 'ARMATURE':
     symmetrize_bone_names(obj)
+    change_root_bone_shape(obj)
     gen_finger_ctrl(obj, 'Thumb', 'L', '-Z')
     gen_finger_ctrl(obj, 'Thumb', 'R', 'Z')
     for side in ['L', 'R']:
         for finger in ['Index', 'Middle', 'Ring', 'Little']:
             gen_finger_ctrl(obj, finger, side, 'X')
-        #gen_limbs_ik(obj, 'Arm', side)
-        #gen_limbs_ik(obj, 'Leg', side)
-        #fix_arm_twist(obj, side)
-    #set_spring_bones(obj)
+        gen_limbs_ik(obj, 'Arm', side)
+        gen_limbs_ik(obj, 'Leg', side)
+        fix_arm_twist(obj, side)
+    set_spring_bones(obj)
        
     
